eceec32394572f5d77e603c421f2c37f0d4e22a6
justine-lorenc <lorencjustine@gmail.com>
2023-02-08 18:48:08 -0600

    Initial layout.

--- BookReviews.Web/App_Start/BundleConfig.cs
+++ BookReviews.Web/App_Start/BundleConfig.cs
@@ -19,12 +19,12 @@ namespace BookReviews.Web
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/app.css"));
         }
     }
 }
